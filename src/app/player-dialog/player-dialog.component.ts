201d523973c76070bf1082b3d05c63104328725f
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-08 14:16:48 -0500

    Implementación de nuevo jugador a la tabla 1

---
+++ src/app/player-dialog/player-dialog.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Country, SquadNumber } from '../interfaces/player';
+import { Team } from '../interfaces/team';
+import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+
+@Component({
+  selector: 'app-player-dialog',
+  templateUrl: './player-dialog.component.html',
+  styleUrls: ['./player-dialog.component.scss']
+})
+export class PlayerDialogComponent implements OnInit {
+
+  private team!: Team;
+  public countries = Object.keys(Country).map(key => ({ label: key, key: (Country as any)[key]}));
+  public squadNumber = Object.keys(SquadNumber)
+    .slice(Object.keys(SquadNumber).length / 2)
+    .slice(Object.keys(SquadNumber).length / 2)
+    .map(key => ({
+      label: key,
+      key: SquadNumber[key as any]
+    }));
+
+  constructor(private playerService: PlayerService, private teamService: TeamService) { }
+
+  ngOnInit(): void {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        if (teams.length > 0) {
+          this.team = teams[0];
+        }
+      });
+  }
+
+  private newPlayer(playerFormValue: any) {
+    const key = this.playerService.addPlayer(playerFormValue).key;
+    const playerFormValueKey = {
+      ...playerFormValue,
+      key
+    };
+    const formattedTeam = {
+      ...this.team,
+      players: [...(this.team.players ? this.team.players : []), playerFormValueKey]
+    };
+
+    this.teamService.editTeam(formattedTeam);
+  }
+
+}
---
+++ src/app/player-table/player-table.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Player } from '../interfaces/player';
+import { PlayerService } from '../services/player.service';
+
+@Component({
+  selector: 'app-player-table',
+  templateUrl: './player-table.component.html',
+  styleUrls: ['./player-table.component.scss']
+})
+export class PlayerTableComponent implements OnInit {
+   public players$!: Observable<Player[]>;
+   public selectedPlayer!: Player;
+
+   public showModal = false;
+
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit(): void {
+    this.players$ = this.playerService.getPlayers(); // Obtenemos la coleccion de jugadores
+  }
+
+  newPlayer() {
+    this.showModal = true;
+    this.selectedPlayer;
+
+    setTimeout(() => {
+      window.location.replace('#open');
+    });
+  }
+
+}
